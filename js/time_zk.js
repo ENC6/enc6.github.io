c7f2f43c6bf7092b6ae3e7c49792be48233f6e6e
ENC6 <2836636143@qq.com>
2021-02-02 18:29:31 +0800

    改动

--- js/time_zk.js
+++ js/time_zk.js
@@ -13,6 +13,6 @@ hrsold=Math.floor(e_hrsold);
 e_minsold=(e_hrsold-hrsold)*60;
 minsold=Math.floor((e_hrsold-hrsold)*60);
 seconds=Math.floor((e_minsold-minsold)*60);
-span_dt_dt.innerHTML="<div height="50"><font size=5.2 face='ff'>距</font><font size=5.2 face='ff' color=red>中考</font><font size=5.2 face='ff'>还有：</div></font><font size=8 face='led'>"+daysold+"</font><font size=8 face='ff'>天</font><font size=8 face='led'>"+hrsold+"</font><font size=8 face='ff'>小时</font><font size=8 face='led'>"+minsold+"</font><font size=8 face='ff'>分</font><font size=8 face='led'>"+seconds+"</font><font size=8 face='ff'>秒</font>"+"<br></font><br>" ; // 这里你自己改。就是倒计时的样式，字体大小size=4 颜色是color
+span_dt_dt.innerHTML="<div><font size=5.2 face='ff'>距</font><font size=5.2 face='ff' color=red>中考</font><font size=5.2 face='ff'>还有：</div></font><font size=8 face='led'>"+daysold+"</font><font size=8 face='ff'>天</font><font size=8 face='led'>"+hrsold+"</font><font size=8 face='ff'>小时</font><font size=8 face='led'>"+minsold+"</font><font size=8 face='ff'>分</font><font size=8 face='led'>"+seconds+"</font><font size=8 face='ff'>秒</font>"+"<br></font><br>" ; // 这里你自己改。就是倒计时的样式，字体大小size=4 颜色是color
 }
 show_date_time();
